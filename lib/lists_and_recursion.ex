c07d5ab4c273f3e64df84e05e6f8878b3d7c7524
Santiago Torres G <thdealer@gmail.com>
2020-04-27 21:36:55 -0500

    Add my_max function using recursion

--- lib/lists_and_recursion.ex
+++ lib/lists_and_recursion.ex
@@ -1,12 +1,13 @@
 defmodule ListAndRecursion do
-  defp map([], _func), do: []
 
+  # ===== ➤ EXERCISE: ListAndRecursion-1 (Pag. 77) ======
+
+  defp map([], _func), do: []
   defp map([head | tail], func) do
     [func.(head) | map(tail, func)]
   end
 
   defp sum([], acum), do: acum
-
   defp sum([head | tail], acum) do
     sum(tail, head + acum)
   end
@@ -16,4 +17,12 @@ defmodule ListAndRecursion do
     map(list, func)
     |> sum(0)
   end
+
+
+  # ===== ➤ EXERCISE: ListAndRecursion-2 (Pag. 77) ======
+
+  def my_max(list, max_value \\ 0)
+  def my_max([], max_value), do: max_value
+  def my_max([head | tail], max_value) when head >= max_value, do: my_max(tail, head)
+  def my_max([head | tail], max_value) when head <= max_value, do: my_max(tail, max_value)
 end
